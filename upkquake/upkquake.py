732009f28cc4bb109b2c74047af5549df728bfe1
Ian McGaunn <ianmcgaunn@gmail.com>
2020-08-19 15:39:58 -0400

    start work on new feature

--- upkquake/upkquake.py
+++ upkquake/upkquake.py
@@ -1,5 +1,7 @@
 import logging
+import os
 import sys
+import tempfile
 import time
 
 import upkquake.constants as constants
@@ -14,7 +16,13 @@ logger = logging.getLogger('upkquake-main')
 
 
 def main(args=[]):
+    # TODO: create a temporary directory to house all intermediate files/
+    # to cache the dependencies from archive.org
+    cache_dir = os.path.join(os.getenv('HOME'), '.cache', 'upkquake')
+    util.mkdir_if_notexists(cache_dir)
     if not args:
+        # before downloading, check if zip is already downloaded.
+        # archive.org is slow and it's nice to have this cached
         with ThreadPoolExecutor() as tpe:
             download_handle = tpe.submit(extraction.download_q2_zip)
             spinner = util.spinning_cursor()
